c3e0388903b369e045ccbd49add53a2160602aca
Kelly Dresser <kdresser@shaw.ca>
2013-07-11 18:56:56 -0700

    maybe simpler; full async

--- grader.js
+++ grader.js
@@ -1,50 +1,52 @@
 #!/usr/bin/env node
-var fs = require('fs');
+var fs      = require('fs'       );
 var program = require('commander');
 var cheerio = require('cheerio'  );
 var restler = require('restler'  );
 var util    = require('util'     );
 var CHECKSFILE_DEFAULT = "checks.json";
-var HTMLFILE_DEFAULT = "";                //"index.html";
-var HTMLURL_DEFAULT = "";                 //"http://google.com";
+var HTMLFILE_DEFAULT = "index.html"; 
+var HTMLURL_DEFAULT = "";            
 
-var existsFile = function(pfn) {
-  if (pfn == '') { return false };
-  return (fs.existsSync(pfn));
+var getFileSync = function(pfn) {
+  // Return empty string if file DNE.
+  if (!fs.existsSync(pfn)) {  return '' };
+  return fs.readFileSync(pfn);
 };
 
-var getFile = function(pfn) {
-  if (existsFile(pfn))   { return fs.readFileSync(pfn) }
-  else                   { return '' };
-};
-
-var cheerioString = function(str) {
-  return cheerio.load(str);
-}
-
-var loadChecks = function(checksfile) {
-    return JSON.parse(fs.readFileSync(checksfile));
-};
-
-var checkHTML = function(html, checksfile) {
-    $ = cheerioString(html);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
+var checkHtmlString = function(htmlstr, chksfile) {
+    // It's late, but complain and exit if chksfile DNE.
+    if (!fs.existsSync(program.checks)) {
+        console.log("Checks file '%s' DNE", chksfile);
+        return;
+    }
+    $ = cheerio.load(htmlstr);
+    var checkJson = {};
+    var checks = JSON.parse(getFileSync(chksfile)).sort();
     for (var ii in checks) {
         var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+        checkJson[checks[ii]] = present;
     }
-    return out;
-};
-
-var doHTML = function(html, checksfile) {
-  var checkJson = checkHTML(html, program.checks);
   var outJson = JSON.stringify(checkJson, null, 4);
   console.log(outJson);
+}
+
+var processFile = function(pfn, chksfile) {
+  if (fs.existsSync(pfn))   { 
+    fs.readFile(pfn, function(err, data) {
+      if (err) {
+        console.log("Error reading '%s'", pfn);
+      } else {
+        checkHtmlString(data, chksfile);  
+      };
+    });
+  } else {
+    console.log("File '%s' DNE", pfn);  
+  }
 };
 
-var buildCrunch = function(checksfile) {
-    var f = function(result, response) {
+var processURL = function(url, chksfile) {
+  restler.get(url).on('complete', function(result, response) {
         if (result instanceof Error) {
             console.log('Error: ' + util.format(result.message));
             return;
@@ -53,41 +55,30 @@ var buildCrunch = function(checksfile) {
             console.log('Error: page not found');
             return;
         }
-        doHTML(result, checksfile);
-    };
-    return f;
+        checkHtmlString(result, chksfile);
+  });
 };
 
-if(require.main == module) {
+if (require.main == module) {
+    // Get the options, without anything fancy checking.
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', String, CHECKSFILE_DEFAULT)
         .option('-f, --file   <html_file>',  'Path to index.html',  String, HTMLFILE_DEFAULT  )
         .option('-u, --url    <html_url>',   'URL to index.html',   String, HTMLURL_DEFAULT  )
         .parse(process.argv);
     //
-    // Checks?
-    if (!existsFile(program.checks)) {
-        console.log("checks file '%s' does not exist. Exiting.", program.checks);
-        process.exit(1); 
-    }
-    //
-    // File?
-    if (program.file != '') {
-      if (!existsFile(program.file)) {
-          console.log("html file '%s' does not exist. Exiting.", program.file);
-          process.exit(1); 
-      }
-      doHTML(getFile(program.file), program.checks);
-      process.exit(0);
-    }
-    //
-    // URL?
+    // Check a URL? (Do this first, bcs file defaults).
     if (program.url != '') {
-      var doCrunch = buildCrunch(program.checks);
-      restler.get(program.url).on('complete', doCrunch);
-      // Wait.
+      program.file = '';            // Prevent double checks.
+      processURL(program.url, program.checks);
     }
-    // Wait.
+    //
+    // Check a file?
+    if (program.file != '') {
+      processFile(program.file, program.checks);
+    }
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    exports.checkProcessFile = checkProcessFile;
+    exports.checkProcessURL = checkProcessURL;
+    exports.checkHtmlString = checkHtmlString;
 }
